d27701e3e6214a55e43a6c5900464cbbfecb40a7
Alexander Hartelt <hartelt.alexander@gmail.com>
2018-12-17 19:42:17 +0100

    Added requirement.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='layout_analysis',
+    version='0.0.1',
+    packages=find_packages(),
+    license='LGPL-v3.0',
+    long_description=open("README.md").read(),
+    include_package_data=True,
+    author="Alexander Hartelt, Christoph Wick",
+    author_email="christoph.wick@informatik.uni-wuerzburg.de",
+    url="https://gitlab2.informatik.uni-wuerzburg.de/OMMR4all/ommr4all-layout-analysis.git",
+    download_url='https://gitlab2.informatik.uni-wuerzburg.de/OMMR4all/ommr4all-layout-analysis.git',
+    install_requires=open("requirements.txt").read().split(),
+    keywords=['OMR', 'layout analysis', 'pixel classifier'],
+    data_files=[('', ["requirements.txt"])],
+)
