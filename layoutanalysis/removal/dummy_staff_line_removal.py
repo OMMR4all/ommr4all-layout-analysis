aa8ef3d868bc85ae57e2ba516923084595a4436c
Alexander Hartelt <hartelt.alexander@gmail.com>
2019-01-06 02:14:08 +0100

    changes to sstaff_line_removal

--- layoutanalysis/removal/dummy_staff_line_removal.py
+++ layoutanalysis/removal/dummy_staff_line_removal.py
@@ -1,20 +1,28 @@
 import numpy as np
 from scipy.interpolate import interpolate
-from skimage import morphology, measure
 
 def staff_removal(staffs_lines, img, line_height):
     nimg = np.copy(img)
     h = nimg.shape[0]
-    w = nimg.shape[1]
     for system in staffs_lines:
-        for staff in system:
+        for ind, staff in enumerate(system):
             y, x = zip(*staff)
             f = interpolate.interp1d(x, y)
-            x_start, x_end = x[0], x[-1]
+            x_start, x_end = min(x), max(x)
             for i in range(x_start, x_end):
                 count = []
-                yt = int(f(i))
-                yb = int(f(i))
+                l2 = line_height // 2
+                st_point = int(f(i))
+                if nimg[st_point][i] != 0:
+                    for z in range(1, l2 + 1):
+                        if nimg[st_point - z][i] == 0:
+                            st_point = st_point-z
+                            break
+                        if nimg[st_point + z][i] == 0:
+                            st_point = st_point-z
+                            break
+                yt = st_point
+                yb = st_point
                 if nimg[yt][i] == 0:
                     count.append(yt)
                     while yt < h - 1:
@@ -32,8 +40,4 @@ def staff_removal(staffs_lines, img, line_height):
                 if len(count) <= line_height:
                     for it in count:
                         nimg[it][i] = 1
-    # Label all connected components:
-    #all_labels = measure.label(nimg, background = 1)
-    #morphology.remove_small_objects(all_labels, maxNoiseSize, in_place = True)
-    #return np.clip(all_labels, 0, 1)
     return nimg
